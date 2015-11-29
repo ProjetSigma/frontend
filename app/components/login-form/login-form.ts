a40b83a76b4d57744f20c3bec6ef9fd9ddd2a106
Denis Merigoux <denis.merigoux@gmail.com>
2015-11-29 23:55:42 +0100

    creating phantom backend service for authentification

--- app/components/login-form/login-form.ts
+++ app/components/login-form/login-form.ts
@@ -1,13 +1,21 @@
 import {Component} from 'angular2/angular2';
+import {AuthService} from '../../services/users/auth-service';
 
 @Component({
     selector: 'login-form',
-    templateUrl : './components/login-form/login-form.html'
+    templateUrl : './components/login-form/login-form.html',
+    providers: [AuthService]
 })
 export class LoginFormComponent {
     public username;
     public password;
+    private authService:AuthService
+
+    constructor(backend:AuthService) {
+        this.authService = backend
+    }
+
     login(username,password) {
-        console.log(`L'utilisateur essaye de se connecter avec les identifiants :`,username,`et`,password);
+        return this.authService.authentificate(username,password)
     }
 }
---
+++ app/services/users/auth-service.ts
@@ -0,0 +1,5 @@
+export class AuthService {
+    authentificate(username,password) {
+        console.log(`L'utilisateur essaye de se connecter avec les identifiants :`,username,`et`,password);
+    }
+}
