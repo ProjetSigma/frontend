ae363c6c04f29bec50ec8bc3ae3cb3425d647d8f
Denis Merigoux <denis.merigoux@gmail.com>
2015-12-08 22:14:42 +0100

    Encapsulate auth header appending

--- app/services/users/auth-service.ts
+++ app/services/users/auth-service.ts
@@ -40,4 +40,8 @@ export class AuthService {
     isAuthenticated() {
         return this.accessToken !== undefined;
     }
+
+    appendAuth(header:Headers) {
+        header.append('Authorization', 'Bearer ' + this.accessToken);
+    }
 }
