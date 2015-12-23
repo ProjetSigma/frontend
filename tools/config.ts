24df4759fb5ec269828f158e168a4bea38d88094
Basile Bruneau <basilebruneau@gmail.com>
2015-12-23 12:32:20 +0100

    Upgrade to Angular beta-2.0.0

--- tools/config.ts
+++ tools/config.ts
@@ -18,37 +18,57 @@ export const ASSETS_SRC           = `${APP_SRC}/assets/**/*`;
 export const SCSS_INDEX           = ['app/sass/themes/classic.scss'];
 
 export const TOOLS_DIR            = 'tools';
+export const TMP_DIR              = 'tmp';
 export const TEST_DEST            = 'test';
 export const DOCS_DEST            = 'docs';
 export const APP_DEST             = `dist/${ENV}`;
 export const ASSETS_DEST          = `${APP_DEST}/assets`;
+export const BUNDLES_DEST         = `${APP_DEST}/bundles`;
 export const CSS_DEST             = `${APP_DEST}/css`;
 export const FONTS_DEST           = `${APP_DEST}/fonts`;
 export const LIB_DEST             = `${APP_DEST}/lib`;
+export const APP_ROOT             = ENV === 'dev' ? `${APP_BASE}${APP_DEST}/` : `${APP_BASE}`;
 export const VERSION              = appVersion();
 
-export const VERSION_NPM          = '3.0.0';
+export const VERSION_NPM          = '2.14.7';
 export const VERSION_NODE         = '4.0.0';
 
+// Declare NPM dependencies (Note that globs should not be injected).
+export const NPM_DEPENDENCIES = [
+  { src: 'systemjs/dist/system-polyfills.js', dest: LIB_DEST },
 
-export const DEV_DEPENDENCIES = [
-    { src: 'systemjs/dist/system-polyfills.js', dest: LIB_DEST },
+  { src: 'es6-shim/es6-shim.min.js', inject: 'shims', dest: LIB_DEST },
+  { src: 'reflect-metadata/Reflect.js', inject: 'shims', dest: LIB_DEST },
+  { src: 'systemjs/dist/system.src.js', inject: 'shims', dest: LIB_DEST },
+  { src: 'angular2/bundles/angular2-polyfills.js', inject: 'shims', dest: LIB_DEST },
 
-    { src: 'es6-shim/es6-shim.min.js',    dest: LIB_DEST, inject: 'shims' },
-    { src: 'reflect-metadata/Reflect.js', dest: LIB_DEST, inject: 'shims' },
-    { src: 'systemjs/dist/system.src.js', dest: LIB_DEST, inject: 'shims' },
+  // Faster dev page load
+  { src: 'rxjs/bundles/Rx.min.js', inject: 'libs', dest: LIB_DEST },
+  { src: 'angular2/bundles/angular2.min.js', inject: 'libs', dest: LIB_DEST },
+  { src: 'angular2/bundles/router.js', inject: 'libs', dest: LIB_DEST }, // use router.min.js with alpha47
+  { src: 'angular2/bundles/http.min.js', inject: 'libs', dest: LIB_DEST },
 
-    { src: 'angular2/bundles/angular2.dev.js',  dest: LIB_DEST, inject: true },
-    { src: 'angular2/bundles/router.dev.js',    dest: LIB_DEST, inject: true },
-    { src: 'angular2/bundles/http.dev.js',      dest: LIB_DEST, inject: true },
+  { src: 'js-data/dist/js-data.js',      dest: APP_DEST, inject: true },
+  { src: 'js-data-http/dist/js-data-http.js',      dest: APP_DEST, inject: true },
 
-    { src: 'font-awesome/fonts/FontAwesome.otf',   dest: FONTS_DEST},
-    { src: 'font-awesome/fonts/fontawesome-webfont.eot',   dest: FONTS_DEST},
-    { src: 'font-awesome/fonts/fontawesome-webfont.svg',   dest: FONTS_DEST},
-    { src: 'font-awesome/fonts/fontawesome-webfont.ttf',   dest: FONTS_DEST},
-    { src: 'font-awesome/fonts/fontawesome-webfont.woff',  dest: FONTS_DEST},
-    { src: 'font-awesome/fonts/fontawesome-webfont.woff2', dest: FONTS_DEST}
+  { src: 'font-awesome/fonts/FontAwesome.otf',   dest: FONTS_DEST},
+  { src: 'font-awesome/fonts/fontawesome-webfont.eot',   dest: FONTS_DEST},
+  { src: 'font-awesome/fonts/fontawesome-webfont.svg',   dest: FONTS_DEST},
+  { src: 'font-awesome/fonts/fontawesome-webfont.ttf',   dest: FONTS_DEST},
+  { src: 'font-awesome/fonts/fontawesome-webfont.woff',  dest: FONTS_DEST},
+  { src: 'font-awesome/fonts/fontawesome-webfont.woff2', dest: FONTS_DEST}
 ];
+
+// Declare local files that needs to be injected
+export const APP_ASSETS = [
+  { src: `${ASSETS_SRC}/main.css`, inject: true, dest: CSS_DEST }
+];
+
+NPM_DEPENDENCIES
+  .filter(d => !/\*/.test(d.src)) // Skip globs
+  .forEach(d => d.src = require.resolve(d.src));
+
+export const DEPENDENCIES = NPM_DEPENDENCIES.concat(APP_ASSETS);
 
 export const SCSS_DEPENDENCIES = [
     'node_modules/foundation-sites/scss',
@@ -56,15 +76,39 @@ export const SCSS_DEPENDENCIES = [
     'node_modules/font-awesome/scss'
 ];
 
-export const SYSTEM_CONFIG = {
-    defaultJSExtensions: true,
-    paths: { }
+// ----------------
+// SystemsJS Configuration.
+const SYSTEM_CONFIG_DEV = {
+  defaultJSExtensions: true,
+  paths: {
+    'bootstrap': `${APP_ROOT}bootstrap`,
+    '*': `${APP_BASE}node_modules/*`
+  }
+};
+
+const SYSTEM_CONFIG_PROD = {
+  defaultJSExtensions: true,
+  bundles: {
+    'bundles/app': ['bootstrap']
+  }
+};
+
+export const SYSTEM_CONFIG = ENV === 'dev' ? SYSTEM_CONFIG_DEV : SYSTEM_CONFIG_PROD;
+
+// This is important to keep clean module names as 'module name == module uri'.
+export const SYSTEM_CONFIG_BUILDER = {
+  defaultJSExtensions: true,
+  paths: {
+    '*': `${TMP_DIR}/*`,
+    'angular2/*': 'node_modules/angular2/*',
+    'rxjs/*': 'node_modules/rxjs/*'
+  }
 };
 
 
 // --------------
 // Private.
 function appVersion(): number|string {
-    var pkg = JSON.parse(readFileSync('package.json').toString());
-    return pkg.version;
+  var pkg = JSON.parse(readFileSync('package.json').toString());
+  return pkg.version;
 }
