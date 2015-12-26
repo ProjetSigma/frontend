232c4c136248638ad68acb169697a3dbac742eab
Denis Merigoux <denis.merigoux@gmail.com>
2015-12-26 19:31:47 +0100

    Added custom angular pipe to display french phone numbers

--- app/components/users/user-details/profile-display/profile-display.ts
+++ app/components/users/user-details/profile-display/profile-display.ts
@@ -1,11 +1,12 @@
 import {Component,Input} from 'angular2/core';
 
 import {User} from '../../../../services/users/user';
-
+import {PhoneNumberFrenchPipe} from './phone-number-french';
 
 @Component({
     selector: 'profile-display',
-    templateUrl: './components/users/user-details/profile-display/profile-display.html'
+    templateUrl: './components/users/user-details/profile-display/profile-display.html',
+    pipes: [PhoneNumberFrenchPipe]
 })
 export class ProfileDisplayComponent {
     @Input('user') user: User;
