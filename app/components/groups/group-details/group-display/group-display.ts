56ff78713303b7645e17d7905000fdfc12a38eb9
Denis Merigoux <denis.merigoux@gmail.com>
2016-02-06 19:04:20 +0100

    Styled titlebar from group page

--- app/components/groups/group-details/group-display/group-display.ts
+++ app/components/groups/group-details/group-display/group-display.ts
@@ -1,13 +1,29 @@
 import {Component,Input} from 'angular2/core';
-import {NgFor, NgIf} from 'angular2/common';
+import {NgFor, NgIf, NgSwitch, NgSwitchWhen, NgSwitchDefault} from 'angular2/common';
 
 import {Group} from '../../../../services/groups/group';
+import {GroupService} from '../../../../services/groups/group-service';
 
 @Component({
     selector: 'group-display',
     templateUrl: './components/groups/group-details/group-display/group-display.html',
-    directives: [NgFor, NgIf]
+    directives: [NgFor, NgIf, NgSwitch, NgSwitchWhen, NgSwitchDefault],
+    providers: [GroupService]
 })
 export class GroupDisplayComponent {
     @Input('group') group: Group;
+    private resp_school: Group = new Group();
+
+    constructor(public group_service:GroupService) {};
+
+    ngOnChanges() {
+      if (this.group.resp_school != undefined) {
+          this.getResp_school(String(this.group.resp_school));
+      }
+    };
+
+    getResp_school(id: string) {
+        this.group_service.getGroup(id)
+            .subscribe(res => this.resp_school = res.json());
+    }
 }
