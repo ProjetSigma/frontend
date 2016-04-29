8544aa21bf63b231d7f9497b9be40c6d22c3b4bf
tizot <masset.camille@gmail.com>
2016-04-29 20:55:00 +0200

    Add build.fonts tasks. Add FontAwesome font files

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -5,6 +5,11 @@ import {InjectableDependency} from './seed.config.interfaces';
 export class ProjectConfig extends SeedConfig {
   PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
 
+  FONTS_DEST = `${this.APP_DEST}/fonts`;
+  FONTS_SRC = [
+    'node_modules/font-awesome/fonts/**'
+  ];
+
   constructor() {
     super();
     this.APP_TITLE = 'Projet Sigma';
