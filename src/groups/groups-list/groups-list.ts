224a6eb25e2dd6070d6303f221c355a0995ef4bc
Myriam <begel@crans.org>
2016-05-11 22:18:27 +0200

    Design group lists

--- src/groups/groups-list/groups-list.ts
+++ src/groups/groups-list/groups-list.ts
@@ -4,11 +4,12 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 import {APIService} from '../../shared/services/api-service';
 import {GroupInlineDisplayComponent} from '../group-details/group-inline-display/group-inline-display';
+import {SearchBox} from '../../shared/resources/search-box';
 
 @Component({
     selector: 'groups-list',
     templateUrl: './groups/groups-list/groups-list.html',
-    directives: [NgFor, ROUTER_DIRECTIVES, GroupInlineDisplayComponent]
+    directives: [NgFor, ROUTER_DIRECTIVES, GroupInlineDisplayComponent, SearchBox]
 })
 export class GroupsListComponent {
     public groups;
