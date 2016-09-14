77f8e892f68efbe709303eeffb6de6264947c9fd
Myriam Bégel <begel@crans.org>
2016-09-14 15:10:07 +0200

    Search bar for users and groups

--- src/groups/groups-list/groups-list.ts
+++ src/groups/groups-list/groups-list.ts
@@ -4,21 +4,46 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 import {APIService} from '../../shared/services/api-service';
 import {GroupInlineDisplayComponent} from '../group-details/group-inline-display/group-inline-display';
-import {SearchBox} from '../../shared/resources/search-box';
+import {Group} from '../../shared/resources/group';
 
 @Component({
     selector: 'groups-list',
     templateUrl: './groups/groups-list/groups-list.html',
-    directives: [NgFor, ROUTER_DIRECTIVES, GroupInlineDisplayComponent, SearchBox]
+    directives: [NgFor, ROUTER_DIRECTIVES, GroupInlineDisplayComponent]
 })
 export class GroupsListComponent {
-    public groups;
+    private allGroups: Group[] = [];
+    private displayedGroups: Group[] = [];
+    private searchGroup: string = '';
 
     constructor(public api: APIService) {
+        this.allGroups = [];
+        this.displayedGroups = [];
         this.getGroups();
+        this.searchGroup = '';
     };
 
     getGroups() {
-        this.api.store.findAll('group').then(res => this.groups = res);
+        this.api.store.findAll('group').then(res => {
+            this.allGroups = res;
+            this.displayedGroups = res;
+        });
+    }
+
+    updateGroups(searchBar) {
+        this.displayedGroups = this.allGroups;
+
+        var q = searchBar.target.value;
+        if (q.trim() === '') {
+            return;
+        }
+        q = q.toLowerCase();
+
+        this.displayedGroups = this.allGroups.filter((group) => {
+            if (group.name.toLowerCase().indexOf(q) > -1) {
+                return true;
+            }
+            return false;
+        });
     }
 }
--- src/users/users-list/users-list.ts
+++ src/users/users-list/users-list.ts
@@ -5,21 +5,47 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {APIService} from '../../shared/services/api-service';
 import {User} from '../../shared/resources/user';
 import {UserInlineDisplayComponent} from '../user-details/user-inline-display/user-inline-display';
-import {SearchBox} from '../../shared/resources/search-box';
 
 @Component({
     selector: 'users-list',
     templateUrl: './users/users-list/users-list.html',
-    directives: [NgFor, ROUTER_DIRECTIVES, UserInlineDisplayComponent, SearchBox]
+    directives: [NgFor, ROUTER_DIRECTIVES, UserInlineDisplayComponent]
 })
 export class UsersListComponent {
-    public users: User[] = [];
+    private allUsers: User[] = [];
+    private displayedUsers: User[] = [];
+    private searchUser: string = '';
 
     constructor(public api: APIService) {
+        this.allUsers = [];
+        this.displayedUsers = [];
         this.getUsers();
+        this.searchUser = '';
     };
 
     getUsers() {
-        this.api.store.findAll('user').then(res => this.users = res);
+        this.api.store.findAll('user').then(res => {
+            this.allUsers = res;
+            this.displayedUsers = res;
+        });
+    }
+
+    updateUsers(searchBar) {
+        this.displayedUsers = this.allUsers;
+
+        var q = searchBar.target.value;
+        if (q.trim() === '') {
+            return;
+        }
+        q = q.toLowerCase();
+
+        this.displayedUsers = this.allUsers.filter((user) => {
+            if (user.firstname.concat(' '.concat(user.lastname)).toLowerCase().indexOf(q) > -1
+                || user.lastname.concat(' '.concat(user.firstname)).toLowerCase().indexOf(q) > -1
+            ) {
+                return true;
+            }
+            return false;
+        });
     }
 }
