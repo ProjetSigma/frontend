8a841dee0d5b3b93445aa065436533c80c0c9a91
Swynfel <swynfel@gmail.com>
2017-05-08 21:56:58 +0200

    Completed searcher interface

--- src/components/main/group/group.exports.ts
+++ src/components/main/group/group.exports.ts
@@ -2,6 +2,8 @@ import {GroupComponent, GroupResolver} from './group.component';
 import {GroupPublicationsComponent} from './publications/group-publications.component';
 import {PostFormComponent} from './publications/postform/postform.component';
 import {GroupMembersComponent} from './members/group-members.component';
+import {GroupChatComponent} from './chat/group-chat.component';
+import {GroupSettingsComponent} from './settings/group-settings.component';
 
 import { GroupsListComponent } from './groups-list/groups-list.component';
 import { GroupInlineDisplayComponent } from './group-details/group-inline-display/group-inline-display.component';
@@ -12,7 +14,9 @@ export const GroupDeclarations = [
     GroupPublicationsComponent,
     PostFormComponent,
     GroupMembersComponent,
+    GroupChatComponent,
     GroupsListComponent,
+    GroupSettingsComponent,
     GroupInlineDisplayComponent
 ];
 
