8b9b7f8b0c4547e82855a40042d725369ca79f8d
felix <felix.martel@polytechnique.edu>
2017-05-09 15:13:55 +0200

    Fixed repetition bug

--- src/utils/store.ts
+++ src/utils/store.ts
@@ -26,66 +26,66 @@ export class Store {
     private ressources: {[key: string]: Ressource};
     private pending: {[key: string]: Promise<Record>};
     private records: {[key: string]: Record};
-    
+
     constructor(adapter: Adapter) {
         this.adapter = adapter;
         this.records = {};
         this.pending = {};
         this.ressources = {};
     }
-    
+
     addRessource(res: Ressource) {
         this.ressources[res.name] = res;
     }
     getRessource(name: string): Ressource {
         return this.ressources[name];
     }
-    
+
     hashName(params: RESTRequestParams) {
         return this.adapter.buildUrl(params);
     }
-    
+
     get(hash: string) {
         return this.records[hash];
     }
-    
+
     // ---------------------------------------------------------
-    
+
     fetch(baseName: string, id?: string|number, action?: string, resName?: string): Promise<any> {
         let params: RESTRequestParams = {location: baseName, id: id, action: action};
         let recordName = this.hashName(params);
         if(resName === undefined) resName = baseName;
-        
+
         if(this.pending[recordName] !== undefined) {
             return this.pending[recordName];
         }
-        
+
         this.pending[recordName] = this.adapter.rest(params).then((items) => {
             if(items instanceof Array) {
                 let collection = new HashCollection(this, recordName, resName);
                 this.records[recordName] = collection;
-                
+
                 let subFetch: Promise<any>[] = new Array();
                 items.forEach((item) => {
-                    let subRecordName = this.hashName({location: resName, id: item['pk'], action: 'retrieve'});
+                    let subRecordName = this.hashName({location: resName, id: item['pk'] | item['id'], action: 'retrieve'});
                     subFetch.push(this.itemFetched(resName, subRecordName, item, true));
                     collection.add(subRecordName);
                 });
                 return Promise.all(subFetch).then(() => collection);
-                
+
             } else {
                 return this.itemFetched(resName, recordName, items, true);
             }
         });
-        
+
         return this.pending[recordName];
     }
-    
+
     // ---------------------------------------------------------
-    
+
     itemFetched(ressourceName: string, recordName: string, obj: any, partial: boolean): Promise<any> {
         const res = this.ressources[ressourceName];
-        
+
         if(this.records[recordName] === undefined) {
             this.records[recordName] = new (res.klass)(this, recordName, ressourceName);
         }
@@ -94,7 +94,7 @@ export class Store {
         for(const prop in obj) {
             record[prop] = obj[prop];
         }
-        
+
         if(res.subCollections) {
             let subFetch: Promise<any>[] = new Array();
             for(const sub of res.subCollections) {
@@ -111,21 +111,21 @@ export class Store {
             return Promise.resolve(record);
         }
     }
-    
+
     // ---------------------------------------------------------
-    
+
     fetchWithCache(baseName: string, id?: string|number, action?: string, resName?: string) : Promise<any> {
         let params: RESTRequestParams = {location: baseName, id: id, action: action};
         let recordName = this.hashName(params);
-        
+
         if (this.records[recordName] === undefined || this.records[recordName].__.invalidated || this.records[recordName].__.partial) {
             return this.fetch(baseName, id, action, resName);
         }
         return Promise.resolve(this.records[recordName]);
     }
-    
+
     // ---------------------------------------------------------
-    
+
     find(baseName: string, id?: string|number, action?: string, resName?: string) : Promise<any> {
         if(action === undefined) {
             if(id === undefined) {
@@ -142,21 +142,21 @@ export class Store {
                 }
             }
         }
-        
+
         return this.fetchWithCache(baseName, id, action, resName);
     }
-    
+
     findAll(baseName: string) {
         return Promise.resolve([]);
     }
     subFind(baseName: string, id:number|string, action: string) {
         return Promise.resolve([]);
     }
-    
+
     // ---------------------------------------------------------
-    
+
     create() {
-        
+
     }
-    
+
 }
