ba5bab86878ce6d586b12a7d4d68104e4adb810f
felix <felix.martel@polytechnique.edu>
2017-05-09 16:33:49 +0200

    Retrieving upcoming events

--- src/components/navbar_left/incoming-events/incoming-events.component.ts
+++ src/components/navbar_left/incoming-events/incoming-events.component.ts
@@ -12,8 +12,14 @@ export class IncomingEventsComponent {
 
     constructor(public api: APIService) {
         this.api.store.find('event').then(res => {
+          var i = 0;
+          var nbEvents = 5; // Number of events to display
           res.forEach((e) => {
-            this.events.push(e);
+            if (i < nbEvents){
+              this.events.push(e);
+            }
+            console.log(i);
+            i++;
           });
         })
     }
