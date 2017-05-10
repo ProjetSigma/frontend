e50162419aa076afa197c8b4622c0f18423e9788
felix <felix.martel@polytechnique.edu>
2017-05-10 15:41:15 +0200

    Adding basic EventDisplay component

--- src/components/events/event-display/event-display.component.ts
+++ src/components/events/event-display/event-display.component.ts
@@ -1,13 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {APIService} from 'services/api.service';
-
+import { Event } from 'resources/event';
 
 @Component({
     selector: 'event-display',
     templateUrl: 'event-display.component.html',
 })
 export class EventDisplayComponent {
-
+  @Input('event') public event: Event;
     constructor(public api: APIService) { };
 
 }
--- src/components/main/agenda/agenda.component.ts
+++ src/components/main/agenda/agenda.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Event } from 'resources/event';
 
 @Component({
     selector: 'agenda',
@@ -6,7 +7,17 @@ import { Component } from '@angular/core';
 })
 
 export class AgendaComponent {
+  public isSelected : boolean = false;
+  public selectedEvent : Event;
+
+  constructor() {};
   calendarOptions:Object = {
     defaultView: 'month',
+  };
+
+  selectEvent(e: Event){
+    this.selectedEvent = e;
+    this.isSelected = true;
   }
+
 }
